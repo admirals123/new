f3c738873d9fcfaa68f42b341aceeedd3f832ea3
admirals123 <158317715+admirals123@users.noreply.github.com>
2024-05-20 18:25:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
@@ -10,3 +13,14 @@ roleRef:
   kind: ClusterRole
   name: kyverno-cleanup-pods
   apiGroup: rbac.authorization.k8s.io
+
+
+
+
+
+DefaultAzureCredential simplifies Azure authentication by trying different methods in order:
+ * Environment Variables: Checks for Azure credentials set as environment variables.
+ * Managed Identity: Uses the identity assigned to an Azure resource (if running on one).
+ * Shared Token Cache:  Looks for credentials in a shared cache (e.g., used by Visual Studio).
+ * Interactive Login:  If none of the above work, prompts the user to log in interactively.
+This allows your code to work seamlessly in different environments without manual credential configuration.
