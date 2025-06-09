c2c69bc962287fae10252a835ad13bb9aa032692
admirals123 <158317715+admirals123@users.noreply.github.com>
2025-06-08 22:44:55 -0400

    Update test-redis.js

--- test-redis.js
+++ test-redis.js
@@ -1,4 +1,9 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-choco install kubernetes-helm
+Key Responsibilities:
+	•  Build and maintain high-performance, fault-tolerant, secure, and scalable platforms.
+	•  Design, build, and maintain the technology platform’s features and infrastructure, including hardware, software, and network components.
+	•  Monitor and evaluate platforms to identify potential issues and make recommendations, including implementing monitoring tools, setting up alerts, and performing maintenance tasks.
+	•  Resolve persistent platform issues surfaced by technical support teams, such as bottlenecks, connectivity problems, and system failures.
+	•  Ensure compliance with regulatory requirements throughout the platform lifecycle, implementing security measures like access controls, encryption, and vulnerability assessments when applicable.
+	•  Partner with architects and business leaders to design and build robust platforms across data, orchestration, and presentation layers on a specific domain.
+	•  Contribute to driving the storage layer and API features roadmap
 
