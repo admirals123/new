3c2faceb4f365c46dbba282ba3c076e544c5b37c
admirals123 <158317715+admirals123@users.noreply.github.com>
2025-07-22 15:16:35 -0400

    Update test-redis.js

--- test-redis.js
+++ test-redis.js
@@ -1,1 +1,14 @@
-az resource list --tag owner=GWAM_TECH --resource-type Microsoft.ManagedIdentity/userAssignedIdentities --query "[].{Name:name, ResourceGroup:resourceGroup, Location:location, ID:id}" -o table
+# Use an official Python runtime as the base image
+FROM python:3.9-slim
+
+# Set working directory
+WORKDIR /app
+
+# Copy the Python script to the container
+COPY script.py .
+
+# Install PyMongo
+RUN pip install pymongo
+
+# Run the script
+CMD ["python", "script.py"]
