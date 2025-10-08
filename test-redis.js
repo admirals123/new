37f199d202b44add59bd0d5a5a4d69052bf7624e
admirals123 <158317715+admirals123@users.noreply.github.com>
2025-10-08 11:47:27 -0400

    Update test-redis.js

--- test-redis.js
+++ test-redis.js
@@ -1,24 +1,34 @@
-apiVersion: v1
-kind: Pod
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
 metadata:
-  name: test-pod
-  namespace: ns-gwam-vonne-dev
+  name: azurekeyvaultsecrets.spv.no
 spec:
-  containers:
-  - name: test-container
-    image: caksprodaccacr.azurecr.io/ns-gwam-dotnet-sb-helloworld:d19f713ae31b375285dca7f1196c74d2b3f6c-882b37772-138  # Replace with your approved registry
-    resources:
-      requests:
-        cpu: "100m"
-        memory: "128Mi"
-      limits:
-        cpu: "200m"
-        memory: "256Mi"
-    volumeMounts:
-    - name: pvc-volume
-      mountPath: "/mnt/data"  # Adjust this path as needed
-  volumes:
-  - name: pvc-volume
-    persistentVolumeClaim:
-      claimName: your-pvc-name  # Replace with your actual PVC name
-  restartPolicy: Always
+  group: spv.no
+  versions:
+    - name: v2beta1
+      served: true
+      storage: true
+      schema:
+        openAPIV3Schema:
+          type: object
+          properties:
+            spec:
+              type: object
+              properties:
+                vaultName:
+                  type: string
+                secretName:
+                  type: string
+                tenantId:
+                  type: string
+              required:
+                - vaultName
+                - secretName
+                - tenantId
+  scope: Namespaced
+  names:
+    plural: azurekeyvaultsecrets
+    singular: azurekeyvaultsecret
+    kind: AzureKeyVaultSecret
+    shortNames:
+      - akvs
