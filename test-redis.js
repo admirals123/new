baf3dcd71f98b2b14f5a4e478739e61f90bf0d6b
admirals123 <158317715+admirals123@users.noreply.github.com>
2025-11-03 14:58:55 -0500

    Update test-redis.js

--- test-redis.js
+++ test-redis.js
@@ -1,34 +1,5 @@
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  name: azurekeyvaultsecrets.spv.no
-spec:
-  group: spv.no
-  versions:
-    - name: v2beta1
-      served: true
-      storage: true
-      schema:
-        openAPIV3Schema:
-          type: object
-          properties:
-            spec:
-              type: object
-              properties:
-                vaultName:
-                  type: string
-                secretName:
-                  type: string
-                tenantId:
-                  type: string
-              required:
-                - vaultName
-                - secretName
-                - tenantId
-  scope: Namespaced
-  names:
-    plural: azurekeyvaultsecrets
-    singular: azurekeyvaultsecret
-    kind: AzureKeyVaultSecret
-    shortNames:
-      - akvs
+FROM Metric
+SELECT average(vpa_%.value)
+FACET namespace, podName, containerName, metricName
+WHERE namespace IS NOT NULL
+TIMESERIES
