9849a899ac5f2953921d01521cf6925faed80a34
admirals123 <158317715+admirals123@users.noreply.github.com>
2025-11-04 14:19:40 -0500

    Update test-redis.js

--- test-redis.js
+++ test-redis.js
@@ -1,5 +1,6 @@
-FROM Metric
-SELECT average(vpa_%.value)
-FACET namespace, podName, containerName, metricName
-WHERE namespace IS NOT NULL
-TIMESERIES
+SELECT average(cpuUsedCores) AS 'Avg CPU Usage (cores)',
+       average(memoryUsedBytes) / 1073741824 AS 'Avg Memory Usage (GiB)'
+FROM K8sContainerSample
+WHERE clusterName = 'your-dev-cluster-name'
+FACET podName, containerName
+SINCE 7 days ago
